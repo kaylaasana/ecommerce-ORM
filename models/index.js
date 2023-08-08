59fda3434817e09672c4e2fefc86d9d80878fe60
kayla <kayladatte@gmail.com>
2023-08-07 17:56:27 -0700

    created relationships on index.js in models directory

--- models/index.js
+++ models/index.js
@@ -5,12 +5,19 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.hasOne(Category, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Tag.belongsToMany(Product, { through: ProductTag });
 // Tags belongToMany Products (through ProductTag)
+Product.belongsToMany(Tag, { through: ProductTag });
 
 module.exports = {
   Product,
